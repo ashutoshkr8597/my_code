b74390e747c6d3cab6a2efdb2ef9a41d122bceef
Ashutosh <ashutoshkr8597@gmail.com>
2022-02-02 11:08:28 +0530

    third commit

---
+++ Conversion.cs
@@ -0,0 +1,8 @@
+int myInt = 10;
+double myDouble = 5.25;
+bool myBool = true;
+
+Console.WriteLine(Convert.ToString(myInt));  
+Console.WriteLine(Convert.ToDouble(myInt)); 
+Console.WriteLine(Convert.ToInt32(myDouble));
+Console.WriteLine(Convert.ToString(myBool));
