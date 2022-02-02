0481830ad2adc0547726f213540052bd09740445
Ashutosh <ashutoshkr8597@gmail.com>
2022-02-02 08:19:23 +0530

    second commit

---
+++ odd_even.cs
@@ -0,0 +1,11 @@
+int num;
+Console.Write("Enter the number");
+num = int.Parse(Console.ReadLine());
+if(num%2 == 0)
+ {
+ Console.WriteLine("Entered number is even number");
+ }
+ else
+ {
+ Console.WriteLine("Entered number is odd");
+ }
