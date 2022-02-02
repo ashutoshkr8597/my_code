20fbda17466f5c158a6a520a70f8843830e3e233
Ashutosh <ashutoshkr8597@gmail.com>
2022-02-02 08:02:39 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+string fname,lname;
+Console.Write("Enter the first Name : ");
+fname = Console.ReadLine();
+Console.Write("Enter the second name : ");
+lname = Console.ReadLine();
+Console.WriteLine("You full name : "+fname+" " +lname);
+
